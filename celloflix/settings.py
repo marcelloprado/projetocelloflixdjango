7144e02da1bfbbb942010818c94a5372095039a8
Marcello Prado <marcellopradomuller@gmail.com>
2024-12-17 06:33:37 -0300

    problemas conexão co o DB

--- celloflix/settings.py
+++ celloflix/settings.py
@@ -89,7 +89,8 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL = "Postgres.DATABASE_URL"
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
