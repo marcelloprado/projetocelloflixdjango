3c67862ffb69b10b70846a9ad71bd2816b28a93f
Marcello Prado <marcellopradomuller@gmail.com>
2024-12-17 06:59:38 -0300

    usando a URL completa ao inves da variavel de ambiente

--- celloflix/settings.py
+++ celloflix/settings.py
@@ -90,7 +90,7 @@ import dj_database_url
 import os
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgres://projetocelloflixdjango-production.up.railway.app'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
