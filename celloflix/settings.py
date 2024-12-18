a0f4f0be8b7bf42196f59e11812225cf545c2baa
Marcello Prado <marcellopradomuller@gmail.com>
2024-12-18 07:39:08 -0300

    ajustando B.D

--- celloflix/settings.py
+++ celloflix/settings.py
@@ -90,7 +90,7 @@ import dj_database_url
 import os
 
 
-DATABASE_URL = 'Postgres.DATABASE_URL'
+DATABASE_URL = 'postgresql://postgres:EWOVHrqCMFuoTBcrpOmfyLNQswmLWrbj@postgres.railway.internal:5432/railway'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
