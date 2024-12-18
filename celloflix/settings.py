8c4d5940cd785d611d69865762d687160cd18e1d
Marcello Prado <marcellopradomuller@gmail.com>
2024-12-18 08:16:28 -0300

    server error 500

--- celloflix/settings.py
+++ celloflix/settings.py
@@ -94,9 +94,7 @@ DATABASES = {
 
 import dj_database_url
 
-
-
-DATABASE_URL = 'postgresql://postgres:EWOVHrqCMFuoTBcrpOmfyLNQswmLWrbj@postgres.railway.internal:5432/railway'
+DATABASE_URL = 'postgresql://postgres:EWOVHrqCMFuoTBcrpOmfyLNQswmLWrbj@autorack.proxy.rlwy.net:40219/railway'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
